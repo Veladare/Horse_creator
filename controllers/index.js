4039165d86f85ef1dbb8e13818bc22d8e57279ef
Jean-Luc <jeanlucv@berkeley.edu>
2023-08-10 21:20:24 -0700

    It should work for now.

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api');
+const mainRoutes = require('./mainRoutes')
+
+
+router.use('/', mainRoutes)
 router.use('/api', apiRoutes);
 
 module.exports = router;
